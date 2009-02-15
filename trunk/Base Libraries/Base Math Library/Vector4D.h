0fa736e5dcaef252b08c8f2023439dc24505372e
bogdencmc@inbox.ru <bogdencmc@inbox.ru@a7a0c544-f853-11dd-a8e2-f7cb3c35fcff>
2009-02-15 20:26:42 +0000

    Implementation of the Vector4D class was added.

--- trunk/Base Libraries/Base Math Library/Vector4D.h
+++ trunk/Base Libraries/Base Math Library/Vector4D.h
@@ -1,8 +1,216 @@
 #pragma once
 
-class Vector4D
+namespace Math
 {
-public:
-	Vector4D(void);
-	~Vector4D(void);
-};
+	class Vector4D
+	{
+		public:
+
+			//---------------------------------- Public Fields ------------------------------------
+
+			float X;
+
+			float Y;
+
+			float Z;
+
+			float W;
+
+			//--------------------------------- Public Constants ----------------------------------
+
+			static Vector4D Zero;
+
+			static Vector4D Unit;
+
+			static Vector4D AxisX;
+
+			static Vector4D AxisY;
+
+			static Vector4D AxisZ;
+
+			static Vector4D AxisW;
+
+			//---------------------------------- Constructors -------------------------------------
+
+			Vector4D ( float x, float y, float z, float w )
+			{
+				X = x;
+
+				Y = y;
+
+				Z = z;
+
+				W = w;
+			}
+
+			//------------------------------- Arithmetic Operators --------------------------------
+
+			friend Vector4D operator + ( const Vector4D& );
+			
+			friend Vector4D operator - ( const Vector4D& );
+
+			friend Vector4D operator + ( const Vector4D&, const Vector4D& );
+			
+			friend Vector4D operator - ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D operator + ( const Vector4D&, float );
+			
+			friend Vector4D operator - ( const Vector4D&, float );
+
+			friend Vector4D operator + ( float, const Vector4D& );
+			
+			friend Vector4D operator - ( float, const Vector4D& );
+			
+			friend Vector4D operator * ( const Vector4D&, const Vector4D& );
+			
+			friend Vector4D operator / ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D operator * ( const Vector4D&, float );
+
+			friend Vector4D operator / ( const Vector4D&, float );	
+
+			friend Vector4D operator * ( float, const Vector4D& );				
+
+			friend Vector4D operator / ( float, const Vector4D& );
+
+			friend Vector4D operator += ( const Vector4D&, const Vector4D& );
+			
+			friend Vector4D operator -= ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D operator += ( const Vector4D&, float );
+
+			friend Vector4D operator -= ( const Vector4D&, float );	
+
+			friend Vector4D operator *= ( const Vector4D&, const Vector4D& );
+			
+			friend Vector4D operator /= ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D operator *= ( const Vector4D&, float );
+
+			friend Vector4D operator /= ( const Vector4D&, float );
+
+			//---------------------------------- Logic Operators ----------------------------------
+
+			friend bool operator < ( const Vector4D&, const Vector4D& );
+
+			friend bool operator > ( const Vector4D&, const Vector4D& );
+
+			friend bool operator < ( const Vector4D&, float );
+
+			friend bool operator > ( const Vector4D&, float );
+
+			friend bool operator < ( float, const Vector4D& );				
+
+			friend bool operator > ( float, const Vector4D& );
+
+			friend bool operator <= ( const Vector4D&, const Vector4D& );
+
+			friend bool operator >= ( const Vector4D&, const Vector4D& );
+
+			friend bool operator <= ( const Vector4D&, float );
+
+			friend bool operator >= ( const Vector4D&, float );
+
+			friend bool operator <= ( float, const Vector4D& );				
+
+			friend bool operator >= ( float, const Vector4D& );
+
+			friend bool operator == ( const Vector4D&, const Vector4D& );
+
+			friend bool operator != ( const Vector4D&, const Vector4D& );
+
+			friend bool operator == ( const Vector4D&, float );
+
+			friend bool operator != ( const Vector4D&, float );
+
+			friend bool operator == ( float, const Vector4D& );
+
+			friend bool operator != ( float, const Vector4D& );
+
+			//-------------------------------- Auxiliary Operators --------------------------------
+
+			operator float * ( )
+			{
+				return &X;
+			}
+
+			float& operator [] ( int index )
+			{
+				return * ( index + &X );
+			}
+
+			//--------------------------------- Common Functions ----------------------------------
+
+			friend Vector4D Abs ( const Vector4D& );
+
+			friend Vector4D Sign ( const Vector4D& );
+
+			friend Vector4D Floor ( const Vector4D& );
+
+			friend Vector4D Fract ( const Vector4D& );
+
+			friend Vector4D Ceiling ( const Vector4D& );
+
+			friend Vector4D Mod ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D Mod ( const Vector4D&, float );
+
+			friend Vector4D Mod ( float, const Vector4D& );
+
+			friend Vector4D Min ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D Min ( const Vector4D&, float );
+
+			friend Vector4D Min ( float, const Vector4D& );
+
+			friend Vector4D Max ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D Max ( const Vector4D&, float );
+
+			friend Vector4D Max ( float, const Vector4D& );
+
+			friend Vector4D Clamp ( const Vector4D&, const Vector4D&, const Vector4D& );
+
+			friend Vector4D Clamp ( const Vector4D&, float, float );
+
+			friend Vector4D Mix ( const Vector4D&, const Vector4D&, const Vector4D& );
+
+			friend Vector4D Mix ( const Vector4D&, const Vector4D&, float );
+
+			friend Vector4D Step ( const Vector4D&, const Vector4D& );
+
+			friend Vector4D Step ( const Vector4D&, float );
+
+			friend Vector4D Smooth ( const Vector4D&, const Vector4D&, const Vector4D& );
+
+			friend Vector4D Smooth ( const Vector4D&, float, float );
+
+			//-------------------------------- Geometric Functions --------------------------------
+
+			friend float Length ( Vector4D& );
+
+			friend float Square ( Vector4D& );
+
+			friend float Distance ( Vector4D&, Vector4D& );
+	        
+			friend float Dot ( Vector4D&, Vector4D& );    
+	        
+			friend Vector4D Normalize ( Vector4D& );
+
+			friend Vector4D Reflect ( Vector4D&, Vector4D& );
+
+			friend Vector4D Refract ( Vector4D&, Vector4D&, float );
+
+			//------------------------------- Exponential Functions -------------------------------
+
+			friend  Vector4D Pow ( Vector4D&, Vector4D& );
+
+			friend  Vector4D Pow ( Vector4D&, float );
+
+			friend  Vector4D Exp ( Vector4D& );
+
+			friend  Vector4D Log ( Vector4D& );
+
+			friend  Vector4D Sqrt ( Vector4D& );
+	};
+}
