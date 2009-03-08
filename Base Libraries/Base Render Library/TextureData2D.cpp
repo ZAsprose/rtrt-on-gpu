660bd64e35acffdaff6208931d74b58fe059ae68
bogdencmc <bogdencmc@inbox.ru>
2009-03-08 21:05:48 +0000

    Added TextureData2D class.

---
+++ Base Libraries/Base Render Library/TextureData2D.cpp
@@ -0,0 +1,77 @@
+#include "TextureData2D.h"
+
+namespace RenderTools
+{
+	//-------------------------------- Constructor and Destructor ---------------------------------
+
+	TextureData2D :: TextureData2D ( int width, int height, int components )
+	{
+		Width = width;
+
+		Height = height;
+
+		Components = components;
+
+		Pixels = new float [ Width * Height * Components ];
+
+		memset ( Pixels, 0, Width * Height * Components );
+	}
+			
+	TextureData2D :: ~TextureData2D ( void )
+	{
+		delete [] Pixels;
+	}
+
+	//---------------------------------------- Data Upload ----------------------------------------
+
+	void TextureData2D :: Upload ( int target )
+	{
+		glTexImage2D ( target,
+			           0,
+					   GetInternalFormat ( ),
+					   Width,
+					   Height,
+					   0,
+					   GetPixelFormat ( ),
+					   GL_FLOAT,
+					   Pixels );
+	}
+	
+	//--------------------------------------- Texture Format --------------------------------------
+	
+	int TextureData2D :: GetPixelFormat ( void )
+	{
+		switch ( Components )
+		{
+			case 1:
+				return GL_ALPHA;
+
+			case 3:
+				return GL_RGB;
+					
+			case 4:
+				return GL_RGBA;
+					
+			default:
+				return -1;
+		}
+	}
+
+	int TextureData2D :: GetInternalFormat ( void )
+	{
+		switch ( Components )
+		{
+			case 1:
+				return GL_ALPHA32F_ARB;
+
+			case 3:
+				return GL_RGB32F_ARB;
+			
+			case 4:
+				return GL_RGBA32F_ARB;
+			
+			default:
+				return -1;
+		}
+	}
+}
---
+++ Base Libraries/Base Render Library/TextureData2D.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#ifndef _TEXTURE_DATA2D_
+
+#define _TEXTURE_DATA2D_
+
+#include <GLee.h>
+
+namespace RenderTools
+{
+	class TextureData2D
+	{
+		private:
+
+			//----------------------------------- Pixels Array ------------------------------------
+
+			float * Pixels;
+
+			//----------------------------------- Texture Size ------------------------------------
+			
+			int Width;
+			
+			int Height;
+
+			//--------------------------------- Pixel Components ----------------------------------
+
+			int Components;
+		
+		public:
+
+			//----------------------------- Constructor and Destructor ----------------------------
+		
+			TextureData2D ( int, int, int = 3 );
+			
+			~TextureData2D ( void );
+
+			//------------------------------------ Data Access ------------------------------------
+
+			operator float * ( void ) { return Pixels; }
+			
+			operator const float * ( void ) const { return Pixels; }
+
+			template < class Type > Type& Pixel ( int, int );
+
+			//------------------------------------ Data Format ------------------------------------
+			
+			int GetPixelFormat ( void );
+
+			int GetInternalFormat ( void );
+			
+			int GetWidth ( void ) { return Width; }
+
+			int GetHeight ( void ) { return Height; }
+
+			int GetComponents ( void ) { return Components; }
+
+			//------------------------------------ Data Upload ------------------------------------
+
+			void Upload ( int target = GL_TEXTURE_2D );
+	};
+
+	//------------------------------------- Template Functions ------------------------------------
+	
+	template < class Type > Type& TextureData2D :: Pixel ( int x, int y )
+	{
+		return ( Type& ) Pixels [ x * Components + y * Width * Components ];
+	}
+}
+
+#endif
