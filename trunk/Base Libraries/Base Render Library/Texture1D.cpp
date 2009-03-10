ed00c189d4d43ebd47d815e89d75f7ea5910a5af
bogdencmc@inbox.ru <bogdencmc@inbox.ru@a7a0c544-f853-11dd-a8e2-f7cb3c35fcff>
2009-03-10 18:35:38 +0000

    Added support of the 1D and 3D textures.

---
+++ trunk/Base Libraries/Base Render Library/Texture1D.cpp
@@ -0,0 +1,82 @@
+#include "Texture1D.h"
+
+#include <iostream>
+
+using namespace std;
+
+namespace RenderTools
+{
+	//-------------------------------- Constructor and Destructor ---------------------------------
+
+	Texture1D :: Texture1D ( int unit, const char * name )
+	{
+		Init ( GL_TEXTURE_1D, NULL, unit, name );
+	}
+				
+	Texture1D :: Texture1D ( TextureData1D * data, int unit, const char * name )
+	{
+		Init ( GL_TEXTURE_1D, data, unit, name );
+	}
+	
+	Texture1D :: ~Texture1D ( void )
+	{
+		delete Data;
+
+		glDeleteTextures ( 1, &Handle );
+	}
+
+	//--------------------------------------- Texture Init ----------------------------------------
+
+	void Texture1D :: Init ( int target, TextureData1D * data, int unit, const char * name )
+	{
+		Target = target;
+
+		Data = data;
+
+		Unit = unit;
+
+		Name = name;
+
+		glGenTextures ( 1, &Handle );
+	}
+
+	//------------------------------------ Texture Management -------------------------------------
+			
+	void Texture1D :: Setup ( void )
+	{
+		cout << "Loading texture data [ \"" << Name << "\" @ " << Unit << " ]" << endl;
+	
+		if ( NULL != Data )
+		{
+			Bind ( );
+
+			glTexParameteri ( Target, GL_TEXTURE_WRAP_S, GL_CLAMP );
+			
+			glTexParameteri ( Target, GL_TEXTURE_WRAP_T, GL_CLAMP );
+
+			glTexParameteri ( Target, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+		    
+			glTexParameteri ( Target, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+			
+			Data->Upload ( Target );
+		}
+		else
+		{
+			cout << "ERROR: Data is empty" << endl;
+		}			                
+	}
+	
+	void Texture1D :: Bind ( void )
+	{
+		glActiveTexture ( GL_TEXTURE0 + Unit );
+		
+		glBindTexture ( Target, Handle );
+	}
+	
+	void Texture1D :: Unbind ( void )
+	{
+		glActiveTexture ( GL_TEXTURE0 + Unit );
+		
+		glBindTexture ( Target, 0 );			
+	}
+}
---
+++ trunk/Base Libraries/Base Render Library/TextureData1D.cpp
@@ -0,0 +1,74 @@
+#include "TextureData1D.h"
+
+namespace RenderTools
+{
+	//-------------------------------- Constructor and Destructor ---------------------------------
+
+	TextureData1D :: TextureData1D ( int width, int components )
+	{
+		Width = width;
+
+		Components = components;
+
+		Pixels = new float [ Width * Components ];
+
+		memset ( Pixels, 0, Width * Components );
+	}
+			
+	TextureData1D :: ~TextureData1D ( void )
+	{
+		delete [] Pixels;
+	}
+
+	//---------------------------------------- Data Upload ----------------------------------------
+
+	void TextureData1D :: Upload ( int target )
+	{
+		glTexImage1D ( target,
+			           0,
+					   GetInternalFormat ( ),
+					   Width,
+					   0,
+					   GetPixelFormat ( ),
+					   GL_FLOAT,
+					   Pixels );
+	}
+	
+	//--------------------------------------- Texture Format --------------------------------------
+	
+	int TextureData1D :: GetPixelFormat ( void )
+	{
+		switch ( Components )
+		{
+			case 1:
+				return GL_ALPHA;
+
+			case 3:
+				return GL_RGB;
+					
+			case 4:
+				return GL_RGBA;
+					
+			default:
+				return -1;
+		}
+	}
+
+	int TextureData1D :: GetInternalFormat ( void )
+	{
+		switch ( Components )
+		{
+			case 1:
+				return GL_ALPHA32F_ARB;
+
+			case 3:
+				return GL_RGB32F_ARB;
+			
+			case 4:
+				return GL_RGBA32F_ARB;
+			
+			default:
+				return -1;
+		}
+	}
+}
