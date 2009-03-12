e169a0414da6fe39caac6db5ee5c86ae8c6ad54b
bogdencmc <bogdencmc@inbox.ru>
2009-03-12 22:09:45 +0000

    Added test implementation of FrameBuffer / ren classes.

--- Base Libraries/Base Render Library/FrameBuffer.h
+++ Base Libraries/Base Render Library/FrameBuffer.h
@@ -1,8 +1,57 @@
 #pragma once
 
-class FrameBuffer
+#ifndef _FRAME_BUFFER_
+
+#define _FRAME_BUFFER_
+
+#include "RenderBuffer.h"
+
+#include "Texture2D.h"
+
+#include <GLee.h>
+
+#include <vector>
+
+using namespace std;
+
+namespace RenderTools
 {
-public:
-	FrameBuffer(void);
-	~FrameBuffer(void);
-};
+	class FrameBuffer
+	{
+		private:
+
+			//----------------------------------- Buffer Handle -----------------------------------
+
+			unsigned int Handle;
+
+		public:
+
+			//------------------------------------ Buffer Size ------------------------------------
+			
+			vector < Texture2D * > ColorBuffers;
+
+			vector < RenderBuffer * > RenderBuffers;
+
+			//--------------------------------- OpenGL Identifier ---------------------------------
+			
+			static const unsigned int Target = GL_FRAMEBUFFER_EXT;
+
+			//----------------------------- Constructor and Destructor ----------------------------
+
+			FrameBuffer ( void );
+
+			~FrameBuffer ( void );
+
+			//----------------------------------- Setup Buffer ------------------------------------
+			
+			void Setup ( void );
+			
+			void Bind ( void );
+			
+			void Unbind ( void );
+
+			void FetchOutcome ( void );
+	};
+}
+
+#endif
