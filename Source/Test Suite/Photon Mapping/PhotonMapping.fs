f83d9a280ed039bd1a957b1fd3ba28a4fb130c75
bogdencmc <bogdencmc@inbox.ru>
2009-06-10 13:10:22 +0000

    Fixed some bugs in photon mapping.

--- Source/Test Suite/Photon Mapping/PhotonMapping.fs
+++ Source/Test Suite/Photon Mapping/PhotonMapping.fs
@@ -26,10 +26,6 @@ struct SIntersection
 	vec3 Point;
 	
 	vec3 Normal;
-	
-
-	//vec3 Color;
-
 };
 
 struct SPlane
@@ -54,10 +50,6 @@ uniform SPlane Plane;
 
 uniform SSphere Sphere;
 
-uniform sampler2DRect Position;
-
-//uniform sampler2DRect Color;
-
 varying vec2 ScreenCoords;
 
 const vec3 AxisX = vec3 ( 1.0, 0.0, 0.0 );
@@ -69,13 +61,6 @@ const vec3 AxisZ = vec3 ( 0.0, 0.0, 1.0 );
 const float PlaneTexScale = 0.25;
 
 //=======================================================================================================================================
-vec3 ChessBoardTexture ( vec3 firstColor, vec3 secondColor, vec2 texcoords )
-{
-	return mix ( firstColor,
-	             secondColor,
-				 0.5 * sign ( ( texcoords.x - 0.5 ) * ( texcoords.y - 0.5 ) ) + 0.5 );
-}
-
 
 SRay GenerateRay ( void )
 {
@@ -91,12 +76,6 @@ bool HitPlane ( in SRay ray, out SIntersection intersection  )
 	intersection.Point = ray.Origin + intersection.Time * ray.Direction;
 	
 	intersection.Normal = vec3 ( 0.0, 1.0, 0.0 );
-		
-	vec2 texcoords = fract ( PlaneTexScale * intersection.Point.xz );
-	
-	//intersection.Color = ChessBoardTexture ( vec3 ( 0.8, 0.8, 0.0 ),
-	//                                         vec3 ( 0.0, 0.0, 0.8 ),
-	//					 texcoords );
 	
 	return ( intersection.Time >= 0.0 ) &&
 	       ( abs ( intersection.Point.x ) <= Plane.Size.x ) &&
@@ -108,10 +87,6 @@ bool HitPlaneEasy ( in SRay ray, out SIntersection intersection  )
 	intersection.Time = ( Plane.Center.y - ray.Origin.y ) / ray.Direction.y;
 	
 	intersection.Point = ray.Origin + intersection.Time * ray.Direction;
-	
-	//intersection.Color = ChessBoardTexture ( vec3 ( 0.8, 0.8, 0.0 ),
-	//                                         vec3 ( 0.0, 0.0, 0.8 ),
-	//					 texcoords );
 	
 	return ( intersection.Time >= 0.0 ) &&
 	       ( abs ( intersection.Point.x ) <= Plane.Size.x ) &&
@@ -137,37 +112,27 @@ bool HitSphere ( in SRay ray, out SIntersection intersection )
 		
 		float tmax = ( -b + det ) / a;
 		
-		if ( tmax > 0.0 )
-		{
-			if ( tmin > 0.0 )
-			{
-				intersection.Time = tmin;
-			}
-			else
-			{
-				intersection.Time = tmax;
-			}
+		intersection.Time = mix ( tmin, tmax, step ( tmin, 0.0 ) );
 			
-			intersection.Point = ray.Origin + intersection.Time * ray.Direction;
+		intersection.Point = ray.Origin + intersection.Time * ray.Direction;
 			
-			intersection.Normal = normalize ( intersection.Point - Sphere.Center );
+		intersection.Normal = normalize ( intersection.Point - Sphere.Center );
 			
-			//intersection.Color = vec3 ( 0.2 );
-			
-			return true;
-		}
+		return tmax > 0.0;
 	}
 	
 	return false;
 }
 
+const float delta = 0.025 / 4.0;
+
 void main( void )
 {
-	SRay ray = GenerateRay();
+	SRay ray = GenerateRay ( );
 	
 	SIntersection intersect;
 	
-	vec3 intens = vec3(0.0);
+	float intens = 0.0;
 	
 	//-------------------------------------------------------------------------
 	
@@ -175,8 +140,7 @@ void main( void )
 	{
 		SIntersection test;
 		
-		if ( HitSphere ( ray, test ) && ( test.Time < intersect.Time ) )
-		
+		if ( HitSphere ( ray, test ) && test.Time < intersect.Time )
 		{
 			vec3 refractDir = refract ( ray.Direction, test.Normal, 1.0 / 1.5 );
 			
@@ -184,9 +148,6 @@ void main( void )
 			
 			if ( HitSphere ( ray, test ) )
 			{
-								 
-				//-------------------------------------------------------------
-				
 				refractDir = refract ( ray.Direction, -test.Normal, 1.5 );
 				
 				ray = SRay ( test.Point + refractDir * 0.001, refractDir );
@@ -195,21 +156,19 @@ void main( void )
 				
 				if ( HitPlaneEasy ( ray, test ) )
 				{
-					//intens += Light.Intens;	
+					intens += delta;	
 					
 					intersect.Point = test.Point;		
 				}
-				
 				else
 				{
-					intersect.Point = vec3(1000000);
+					intersect.Point = vec3 ( 1000000.0 );
 				}
 			}
 		}
 		else
 		{
-			//intens += Light.Intens;
-
+			intens += delta;
 		}
 	}
 	else
@@ -218,28 +177,13 @@ void main( void )
 		
 		if ( HitSphere ( ray, test ) )
 		{
-			ivec2 iii = ivec2 ( ( ScreenCoords + 1.0) * 48 );
-
-			int jjj = iii.x + iii.y;
-
-			vec3 inten111; float ri;
-
-			if ( jjj % 3 == 0) { inten111 = vec3 ( 0.05, 0.0, 0.0 ); ri = 1.50; }
-			else if ( jjj % 2 == 0) { inten111 = vec3 ( 0.0, 0.05, 0.0 ); ri = 1.46; }
-				else { inten111 = vec3 ( 0.0, 0.0, 0.05 ); ri = 1.42; }
-			
-			//-----------------------------------------------------------------
-			
-			vec3 refractDir = refract ( ray.Direction, test.Normal, 1.0 / ri );
+			vec3 refractDir = refract ( ray.Direction, test.Normal, 1.0 / 1.5 );
 			
 			ray = SRay ( test.Point + refractDir * 0.001, refractDir );
 			
 			if ( HitSphere ( ray, test ) )
 			{
-								 
-				//-------------------------------------------------------------
-				
-				refractDir = refract ( ray.Direction, -test.Normal, ri );
+				refractDir = refract ( ray.Direction, -test.Normal, 1.5 );
 				
 				ray = SRay ( test.Point + refractDir * 0.001, refractDir );
 				
@@ -247,26 +191,25 @@ void main( void )
 				
 				if ( HitPlaneEasy ( ray, test ) )
 				{
-					intens += inten111;
+					intens += delta;
 					
 					intersect.Point = test.Point;	
 				}
 				else
 				{
-					intersect.Point = vec3(1000000);
+					intersect.Point = vec3 ( 1000000.0 );
 				}
 			}
 		}
 		else
 		{
-			intersect.Point = vec3(1000000);
+			intersect.Point = vec3 ( 1000000.0 );
 		}
 	}
 	
 	//-------------------------------------------------------------------------
 	
-	//gl_FragData[1] = vec4(intens, 1.0);
-	gl_FragData[0] = vec4( intersect.Point, intens.x );
+	gl_FragColor = vec4 ( intersect.Point, intens );
 }	
 
 
