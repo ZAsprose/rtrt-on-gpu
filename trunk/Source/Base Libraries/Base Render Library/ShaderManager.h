8996d37171d581b2f1e2b12be4aa6f129f6cf8ad
bogdencmc@inbox.ru <bogdencmc@inbox.ru@a7a0c544-f853-11dd-a8e2-f7cb3c35fcff>
2009-10-30 09:33:05 +0000

    Initial commit for new version of ray tracing project!

---
+++ trunk/Source/Base Libraries/Base Render Library/ShaderManager.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   ShaderManager.h
+ * Author: Äåíèñ
+ *
+ * Created on 30 Октябрь 2009 г., 12:18
+ */
+
+#ifndef _SHADERMANAGER_H
+#define	_SHADERMANAGER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _SHADERMANAGER_H */
+
---
+++ trunk/Source/Base Libraries/Base Render Library/Texture3D.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Texture3D.h
+ * Author: Äåíèñ
+ *
+ * Created on 30 Октябрь 2009 г., 12:27
+ */
+
+#ifndef _TEXTURE3D_H
+#define	_TEXTURE3D_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _TEXTURE3D_H */
+
