46630398efb6e2df8a64e5831bc54d72b664cee0
Morozov.Andrey.vmk <Morozov.Andrey.vmk@a7a0c544-f853-11dd-a8e2-f7cb3c35fcff>
2009-11-04 19:05:32 +0000

    Add project Vector for testing class Vector

---
+++ trunk/Research/Vector/build/Debug/GNU-Linux-x86/main.o.d
@@ -0,0 +1,7 @@
+build/Debug/GNU-Linux-x86/main.o: main.cpp vector.hpp config.hpp nvec.hpp
+
+vector.hpp:
+
+config.hpp:
+
+nvec.hpp:
---
+++ trunk/Research/Vector/build/Release/GNU-Linux-x86/main.o.d
@@ -0,0 +1,8 @@
+build/Release/GNU-Linux-x86/main.o: main.cpp vector.hpp config.hpp \
+ nvec.hpp
+
+vector.hpp:
+
+config.hpp:
+
+nvec.hpp:
