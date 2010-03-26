3e777f8d1a66c09fbe9e00a9f454582e083b83fb
morozov.andrey.vmk <morozov.andrey.vmk@a7a0c544-f853-11dd-a8e2-f7cb3c35fcff>
2010-03-26 16:28:53 +0000

    Немного добавил форматирования

--- trunk/Source/Base Libraries/Base Math Library/mmemory.hpp
+++ trunk/Source/Base Libraries/Base Math Library/mmemory.hpp
@@ -1,15 +1,24 @@
-/**
- *** @file mmemory.hpp
- ***
- *** @date 2010
- ***
- *** @version 1.0
- ***
- *** @autor Andrey Morozov
- ***
- *** @email morozov.andrey.vmk@gmail.com
- ***
- **/
+/*
+-----------------------------------------------------------------------------
+|                   B A S E     M A T H     L I B R A R Y                   |
+-----------------------------------------------------------------------------
+
+Andrey Morozov ( morozov.andrey.vmk @ gmail.com )
+Copyright (c) 2009 - 2010
+Denis Bogolepov ( bogdencmc @ inbox.ru )
+
+This library is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this library. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef __mmemory_hpp__
 #define __mmemory_hpp__
